7aa99b44c00bd511f219996a33aecc86f74483ce
Alexander Luna <alexanderfluna@outlook.com>
2023-09-17 10:59:40 -0400

    Updates

--- python/data_structures/hash_table/hash_table.py
+++ python/data_structures/hash_table/hash_table.py
@@ -1,1 +1,25 @@
+class Solution:
+    def twoSum(self, nums, target):
+        hashmap = {}
+        
+        for index, num in enumerate(nums):
+            if num in hashmap:
+                print("Indexes:")
+                print(hashmap[num], index)
 
+                print("Values:")
+                print(nums[hashmap[num]], nums[index])
+
+                return
+            else:
+                hashmap[target - num] = index
+            
+if __name__ == "__main__":
+    s = Solution()
+
+    arr = [1, 2, 3, 4, 5]
+    answer = []
+
+    target = 7
+
+    answer = s.twoSum(arr, target)
