eb25e5d3108c9aec8215ad3e7adec8e2b117e168
Alexander Luna <alexanderfluna@outlook.com>
2023-11-09 13:25:09 -0500

    Modifications

--- java/algorithms/sorting/mergesort.java
+++ java/algorithms/sorting/mergesort.java
@@ -1,59 +1,85 @@
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {12, 11, 13, 5, 6, 7};
-        System.out.println("Original array: " + Arrays.toString(arr));
+        System.out.println("Original array:");
+        for (int i : arr){
+            System.out.print(i + " ");
+        }
 
-        mergeSort(arr);
-
-        System.out.println("Sorted array: " + Arrays.toString(arr));
+        mergeSort(arr, 0, arr.length-1);
+        System.out.println("\n Sorted array:");
+        for (int i : arr){
+            System.out.print(i + " ");
+        }
     }
 
-    public static void mergeSort(int[] arr) {
-        if (arr == null || arr.length <= 1) {
-            return; // Nothing to sort
+    // Input: an unsorted array A[0..n-1]
+    // Input: an integer l, the left-most index
+    // Input: an integer r, the right-most index
+    // Output: the array A, sorted in non-decreasing order
+    public static int[] mergeSort(int[] A, int l, int r) {
+        // Check if there is anything to sort
+        if (l < r) {
+
+            // Compute the middle index
+            int m = (l + r) / 2;
+
+            // Sort the left-hand side
+            mergeSort(A, l, m);
+
+            // Sort the right-hand side
+            mergeSort(A, m + 1, r);
+
+            // Merge them back together
+            merge(A, l, m, r);
+
         }
-
-        int middle = arr.length / 2;
-        int[] left = new int[middle];
-        int[] right = new int[arr.length - middle];
-
-        for (int i = 0; i < middle; i++) {
-            left[i] = arr[i];
-        }
-        for (int i = middle; i < arr.length; i++) {
-            right[i - middle] = arr[i];
-        }
-
-        mergeSort(left);
-        mergeSort(right);
-
-        merge(arr, left, right);
+        return A;
     }
 
-    public static void merge(int[] arr, int[] left, int[] right) {
-        int i = 0, j = 0, k = 0;
+    // Input: an array A[0..n-1]
+    // Input: an integer l, the left-most index
+    // Input: an integer m, the middle index
+    // Input: an integer r, the right-most index
+    public static void merge(int[] A, int l, int m, int r) {
+        // Create a temporary array B
+        int[] B = new int[r - l + 1];
 
-        while (i < left.length && j < right.length) {
-            if (left[i] < right[j]) {
-                arr[k] = left[i];
-                i++;
-            } else {
-                arr[k] = right[j];
-                j++;
+        int i = 0; // First unused index of B
+        int s = l; // Top of the left stack
+        int t = m + 1; // Top of the right stack
+
+        // Keep taking the smallest value as long as both
+        // stacks still have values
+        while (s <= m && t <= r) {
+            if (A[s] <= A[t]) {
+                // Take from left stack
+                B[i] = A[s];
+                s++;
             }
-            k++;
-        }
-
-        while (i < left.length) {
-            arr[k] = left[i];
+            else {
+                // Take from right stack
+                B[i] = A[t];
+                t++;
+            }
             i++;
-            k++;
         }
 
-        while (j < right.length) {
-            arr[k] = right[j];
-            j++;
-            k++;
+        // Copy the remaining elements from the left or right stack, if any
+        while (s <= m) {
+            B[i] = A[s];
+            s++;
+            i++;
+        }
+        while (t <= r) {
+            B[i] = A[t];
+            t++;
+            i++;
+        }
+
+        // Sorting is now complete. Just copy the newly sorted part over the original
+        for (i = 0; i < B.length; i++) {
+            A[l + i] = B[i];
         }
     }
 }
