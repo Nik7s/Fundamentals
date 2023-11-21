d79a8bc90878dd793eaa64f374e758f3082dd1b7
Alexander Luna <59184968+alexanderfluna@users.noreply.github.com>
2023-11-21 17:50:54 -0500

    Delete cpp_CLI directory

--- cpp_CLI/ATM-GUI/BankInterface.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Customer.h";
-using namespace System;
-
-interface class BankInterface{
-	Customer^ FindCustomer(int custNumber, int pin);
-};
--- cpp_CLI/ATM-GUI/README.md
+++
@@ -1,5 +0,0 @@
-# Client_Server
-
-C++/CLI project for Visual Studio with client/server communication.
-
-CLR project with C++/CLI enabled.
